d1a99d0abba948c238bf8e164607a7b7ff98de2b
Abhay Amin <contactabhay2@gmail.com>
2025-12-31 18:31:22 +0530

    Made it module

---
+++ src/app/organizer/[organizerId]/page.tsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Organizers | Operations | ZIZO",
+  description: "View and manage tournament organizers.",
+};
+
+export default function OrganizersPage() {
+  return (
+    <div className="container mx-auto py-10 px-6">
+      <h1>Organizer Details</h1>
+    </div>
+  );
+}
