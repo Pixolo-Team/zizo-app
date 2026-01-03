16ba40ddc54d83b085d6b5087e7e17f4c7c0baab
Jyoti Pandey <jyotipandey@Jyotis-MacBook-Pro.local>
2026-01-03 19:32:15 +0530

    Added the link to navigate to tournaments listing page from everywhere

--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -26,17 +26,18 @@ export default function PageHeader({ title }: PageHeaderProps) {
     <div className="flex justify-between items-start">
       <div className="flex flex-col gap-2.5">
         {/* Brand Logo component */}
-        <BrandLogo
-          variant="text-logo"
-          size={80}
-          className=" block dark-mode-hidden"
-        />
-        <BrandLogo
-          variant="text-logo-white"
-          size={80}
-          className="hidden dark-mode-block"
-        />
-
+        <Link href="/football-tournaments" className="cursor-pointer">
+          <BrandLogo
+            variant="text-logo"
+            size={80}
+            className=" block dark-mode-hidden"
+          />
+          <BrandLogo
+            variant="text-logo-white"
+            size={80}
+            className="hidden dark-mode-block"
+          />
+        </Link>
         {/* Header Text */}
         {title && (
           <p className="text-base text-n-950 font-medium font-gtwalsheim w-3/5 leading-none">
