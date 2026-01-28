2ce8c6418e335624b72a8a6d8a7992d41a6054a2
omisan01 <m.sunesara@somaiya.edu>
2026-01-28 17:35:13 +0530

    Saved Tournaments UI

--- src/app/saved-tournaments/SavedTournaments.tsx
+++ src/app/saved-tournaments/SavedTournaments.tsx
@@ -8,9 +8,6 @@ import { useEffect, useState } from "react";
 import { TournamentListingItemData } from "@/types/tournament";
 
 // COMPONENTS //
-import Motion from "@/components/animations/Motion";
-import TournamentCard from "@/components/tournaments/TournamentCard";
-import Image from "next/image";
 import ShareDrawer from "@/components/drawers/ShareDrawer";
 
 // SERVICES //
@@ -23,7 +20,6 @@ import { useSavedTournaments } from "@/context/SavedTournamentsContext";
 import { trackEvent } from "@/utils/analytics";
 
 // OTHERS //
-import { fadeIn } from "@/lib/animations";
 
 /** Saved Tournaments Page */
 export default function SavedTournaments() {
@@ -31,6 +27,7 @@ export default function SavedTournaments() {
   const router = useRouter();
 
   // Define Context
+  // Saved Tournaments Data
   const { savedTournaments } = useSavedTournaments();
 
   // Define States
@@ -86,69 +83,7 @@ export default function SavedTournaments() {
 
   return (
     <>
-      <div className="h-full pb-20 pt-7 px-5 max-w-[450px] mx-auto relative z-4">
-        {/* Page Title */}
-        <Motion variants={fadeIn} delay={0.1}>
-          <h1 className="text-n-900 text-2xl lg:text-3xl font-semibold mb-6">
-            Saved Tournaments
-          </h1>
-        </Motion>
-
-        {/* Tournament Cards */}
-        <div className="flex flex-col gap-5 lg:gap-10">
-          {savedTournaments.length > 0 ? (
-            savedTournaments.map((tournamentItem, index) => (
-              <Motion key={tournamentItem.tournament_id} variants={fadeIn} delay={0.2 + index * 0.1}>
-                <TournamentCard
-                  tournamentListingItem={tournamentItem}
-                  onShareBtnClick={(event) =>
-                    handleShareBtnClick(event, tournamentItem)
-                  }
-                  onRightArrowClick={() => handleArrowClick(tournamentItem)}
-                />
-              </Motion>
-            ))
-          ) : (
-            /* Empty State */
-            <div className="flex flex-col gap-3 h-full pt-16 lg:pt-0 items-center justify-center">
-              {/* Empty State Image */}
-              <div className="hidden dark-mode-block">
-                <Image
-                  src="/images/no-tournament-dark.png"
-                  alt="No saved tournaments (dark)"
-                  width={1200}
-                  height={120}
-                  priority
-                  className="w-full h-[173px] object-cover invert-[1] lg:h-[280px] xl:h-[350px]"
-                />
-              </div>
-
-              <div className="block dark-mode-hidden">
-                <Image
-                  src="/images/no-tournament-light.png"
-                  alt="No saved tournaments (light)"
-                  width={1200}
-                  height={120}
-                  priority
-                  className="w-full h-[173px] object-cover lg:h-[280px] xl:h-[350px]"
-                />
-              </div>
-
-              <div className="flex flex-col gap-2 items-center">
-                {/* Empty State Title */}
-                <p className="text-center text-n-900 font-medium text-xl lg:text-2xl xl:text-3xl">
-                  No saved tournaments yet
-                </p>
-
-                {/* Empty State Subtitle */}
-                <p className="text-center text-n-600 font-normal leading-[137%] text-sm w-[78%] lg:text-lg xl:text-xl">
-                  Start saving tournaments to see them here
-                </p>
-              </div>
-            </div>
-          )}
-        </div>
-      </div>
+      <div>{/* Tournament Cards */}</div>
 
       {/* SHARE DRAWER */}
       <ShareDrawer
