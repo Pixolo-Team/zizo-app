924a7a0de38b08070cf6e1898b73ff89eeb3d0ec
omisan01 <omkar12mitake@gmail.com>
2026-02-13 18:40:24 +0530

    idetities api

--- src/services/apis/tournaments.api.service.ts
+++ src/services/apis/tournaments.api.service.ts
@@ -4,6 +4,7 @@ import axios, { AxiosRequestConfig } from "axios";
 // TYPES //
 import { ApiResponseData } from "@/types/api";
 import { TournamentListingItemData } from "@/types/tournament";
+import { IdentityData } from "@/types/identities";
 
 // CONSTANTS //
 import { CONSTANTS } from "@/constants";
@@ -23,5 +24,22 @@ export const getTournamentsRequest = async (): Promise<
   // Make API Call
   const response =
     await axios.request<ApiResponseData<TournamentListingItemData[]>>(config);
+  return response.data;
+};
+
+/** Get All Identities API Call */
+export const getAllIdentitiesRequest = async (): Promise<
+  ApiResponseData<IdentityData[]>
+> => {
+  // Set up the API Call Config
+  const config: AxiosRequestConfig = {
+    method: "GET",
+    url: `${CONSTANTS.API_URL}/identities`,
+    headers: {},
+    data: {},
+  };
+
+  // Make API Call
+  const response = await axios.request<ApiResponseData<IdentityData[]>>(config);
   return response.data;
 };
---
+++ src/types/identities.d.ts
@@ -0,0 +1,7 @@
+export interface IdentityData {
+  id: string;
+  name: string;
+  phone: string;
+  first_seen_at: string;
+  last_seen_at: string;
+}
